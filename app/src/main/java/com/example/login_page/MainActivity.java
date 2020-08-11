4de24b0fa2dfde92e9ac99a3375cc18461d3bca2
Rikanthan <rikanthanricky@gmail.com>
2020-08-11 12:00:58 +0530

    signup

--- app/src/main/java/com/example/login_page/MainActivity.java
+++ app/src/main/java/com/example/login_page/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.login_page;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
 import android.widget.Toolbar;
 
 
@@ -13,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    public  void login(View v)
+    {
+        Intent i=new Intent(this,forgetpassword.class);
+        startActivity(i);
 
     }
 }
