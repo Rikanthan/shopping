e5665727c8b67bca9002db54c60d4663ee95c03f
Rikanthan <rikanthanricky@gmail.com>
2020-08-11 22:33:32 +0530

    show password

--- app/src/main/java/com/example/login_page/MainActivity.java
+++ app/src/main/java/com/example/login_page/MainActivity.java
@@ -2,9 +2,30 @@ package com.example.login_page;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import android.content.Intent;
 import android.view.View;
-import android.widget.Toolbar;
+import android.content.res.ColorStateList;
+import android.content.res.XmlResourceParser;
+
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 import android.os.Bundle;
@@ -19,7 +40,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public  void login(View v)
+    public  void signup(View v)
+    {
+        Intent i=new Intent(this,sign_in.class);
+        startActivity(i);
+
+    }
+    public  void forgetpassword(View v)
     {
         Intent i=new Intent(this,forgetpassword.class);
         startActivity(i);
