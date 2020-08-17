3ab97a72d8634a88371b31adfe7bc3fa1aaefa4c
Rikanthan <rikanthanricky@gmail.com>
2020-08-17 12:11:59 +0530

    image view design

--- app/src/main/java/com/example/login_page/customAdapter.java
+++ app/src/main/java/com/example/login_page/customAdapter.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 public class customAdapter extends BaseAdapter {
     private Integer[] items=
-            {R.drawable.fruits,R.drawable.soap,R.drawable.vegetables,R.drawable.education,R.drawable.medicine,R.drawable.fancy,R.drawable.un1,R.drawable.un2};
+            {R.drawable.vegetables,R.drawable.fruit,R.drawable.medical,R.drawable.paper,R.drawable.hygiene,R.drawable.cosmetrics,R.drawable.softdrinks,R.drawable.snacks,R.drawable.bakery,R.drawable.others};
 
     private String[] imageLabels;
     private LayoutInflater thisInflater;
