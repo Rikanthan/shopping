d1d6930a70898e6e053ab5dc8255dbc52cfd5c78
Rikanthan <rikanthanricky@gmail.com>
2020-08-29 23:20:37 +0530

    navigation

--- app/src/main/java/com/example/login_page/admin.java
+++ app/src/main/java/com/example/login_page/admin.java
@@ -1,14 +1,34 @@
 package com.example.login_page;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class admin extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        mDrawer=(DrawerLayout)findViewById(R.id.drawer);
+        mToggle=new ActionBarDrawerToggle(this,mDrawer,R.string.open,R.string.close);
+        mDrawer.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+       if(mToggle.onOptionsItemSelected(item)){
+           return true;
+       }
+
+        return super.onOptionsItemSelected(item);
     }
 }
