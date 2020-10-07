b662236efec2c82f961b9610315f7e729eb3d71d
Rikanthan <rikanthanricky@gmail.com>
2020-10-07 18:59:26 +0530

    floating button design

--- app/src/main/java/com/example/login_page/mango.java
+++ app/src/main/java/com/example/login_page/mango.java
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class mango extends AppCompatActivity {
-TextView t;
-TextView p;
+TextView t,t1;
+TextView p,p1;
 int price=25;
+int price3=60;
 int i=1;
+int j=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mango);
         t=(TextView)findViewById(R.id.mango_quantity);
         p=(TextView)findViewById(R.id.man_price);
+        t1=(TextView)findViewById(R.id.mango3);
+        p1=(TextView)findViewById(R.id.man3_price);
 
     }
     public void max(View v)
@@ -25,14 +32,32 @@ int i=1;
         t.setText(String.valueOf(i));
         p.setText("Price :"+String.valueOf(price*i)+" LKR");
     }
-    public void min(View v)
-    {
-        i=i-1;
-        if(i<1)
-        {
-            i=1;
+    public void min(View v) {
+        i = i - 1;
+        if (i < 1) {
+            i = 1;
         }
         t.setText(String.valueOf(i));
-        p.setText("Price :"+String.valueOf(price*(i))+ " LKR");
+        p.setText("Price :" + String.valueOf(price * (i)) + " LKR");
+    }
+    public void max3(View v)
+    {
+        int i=1;
+        j=j+3;
+        t1.setText(String.valueOf(j));
+        p1.setText("Price :"+String.valueOf(price3*(j/3))+" LKR");
+    }
+    public void min3(View v) {
+        j = j - 3;
+        if (j < 3) {
+            j = 3;
+        }
+        t1.setText(String.valueOf(j));
+        p1.setText("Price :" + String.valueOf(price3 * (j/3)) + " LKR");
+    }
+    public void order(View v)
+    {
+
+        Toast.makeText(getApplicationContext(), "Your order added to cart successfully", Toast.LENGTH_LONG).show();
     }
 }
