eb4700efa972040be2b2bfe118647703acc7c6bf
Rikanthan <rikanthanricky@gmail.com>
2020-10-17 13:08:07 +0530

    image upload v 2.0

---
+++ app/src/main/java/com/example/login_page/Upload.java
@@ -0,0 +1,44 @@
+package com.example.login_page;
+
+public class Upload{
+    private String mName;
+    private String mImageUrl;
+    public Upload()
+    {
+
+    }
+    public Upload(String name, String imageUrl)
+    {
+        if(name.trim().equals(""))
+        {
+            name="No name";
+        }
+        mName=name;
+        mImageUrl=imageUrl;
+    }
+
+
+
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public void setImageUrl(String mImageUrl) {
+        this.mImageUrl = mImageUrl;
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/login_page/imageupload.java
+++ app/src/main/java/com/example/login_page/imageupload.java
@@ -8,175 +8,157 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RadioButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class imageupload extends AppCompatActivity {
-    Button ch,up;
-    ImageView img;
-    DatabaseReference myreff;
-    long imgnewid=0;
-    private EditText productname;
-    private EditText catergory;
-    private EditText quantity;
-    private EditText price;
-    private RadioButton kg;
-    private RadioButton m;
-    private RadioButton l;
-    private RadioButton no;
-    StorageReference mStorageRef;
-    private StorageTask uploadTask;
-    Product product;
-    public Uri imguri;
-    public  String str="";
+    private static final int PICK_IMAGE_REQUEST=1;
+    private Button mButtonChooseImage;
+    private Button mButtonUpload;
+    private TextView mTextViewShowUploads;
+    private EditText mTextFileName;
+    private ImageView mImageView;
+    private ProgressBar mProgressBar;
+    private Uri mImageUri;
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+    private StorageTask mUploadTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imageupload);
-      /*  productname=findViewById(R.id.productname);
-        catergory=findViewById(R.id.catergory);
-        quantity=findViewById(R.id.quantity);
-        price=findViewById(R.id.price);
-        kg=findViewById(R.id.kg);
-        no=findViewById(R.id.other);
-        m=findViewById(R.id.meter);
-        l=findViewById(R.id.litre);
-*/
-        product =new Product();
-        myreff=FirebaseDatabase.getInstance().getReference().child("Product");
-        myreff.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                imgnewid=(snapshot.getChildrenCount());
-            }
+        mButtonChooseImage=findViewById(R.id.button_choose_image);
+        mButtonUpload=findViewById(R.id.button_upload);
+        mTextViewShowUploads=findViewById(R.id.edit_text_file_name);
+        mImageView=findViewById(R.id.image_view);
+        mProgressBar=findViewById(R.id.progress_bar);
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+        mStorageRef= FirebaseStorage.getInstance().getReference("uploads");
+        mDatabaseRef= FirebaseDatabase.getInstance().getReference("uploads");
 
+
+
+        mButtonChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               openFileChooser();
             }
         });
 
-
-        mStorageRef= FirebaseStorage.getInstance().getReference("Images");
-        ch=(Button)findViewById(R.id.choosefile);
-        img=(ImageView)findViewById(R.id.imageupload);
-        up=(Button)findViewById(R.id.upload);
-        ch.setOnClickListener(new View.OnClickListener() {
+        mButtonUpload.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Filechooser();
+            public void onClick(View v) {
+            if(mUploadTask !=null && mUploadTask.isInProgress()){
+                Toast.makeText(imageupload.this,"Upload in progress",Toast.LENGTH_SHORT).show();
             }
-        });
-        up.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(uploadTask !=null && uploadTask.isInProgress())
+
+                 else
                 {
-                Toast.makeText(imageupload.this,"Upload in progress",Toast.LENGTH_LONG).show();
+                    uploadFile();;
                 }
-                else
+            }
 
-                Fileuploader();
+        });
+
+
+        mTextViewShowUploads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
             }
         });
-    }
-    private  String getExtension(Uri uri)
-    {
-        ContentResolver cr=getContentResolver();
-        MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
-        return mimeTypeMap.getExtensionFromMimeType(cr.getType( uri));
-    }
-    private  void Fileuploader()
-    {
-        String imageid;
-        imageid=System.currentTimeMillis()+"."+getExtension(imguri);
-        product.setProductName(productname.getText().toString().trim());
-        product.setCatergory(catergory.getText().toString().trim());
-        //product.setTypequantity(str.trim());
-        int q=Integer.parseInt(quantity.getText().toString().trim());
-        int p=Integer.parseInt(price.getText().toString().trim());
-       // product.setQuantity(q);
-        product.setPrice(p);
-        myreff.push().setValue(product);
 
 
-        StorageReference Ref=mStorageRef.child(imageid);
-       uploadTask= Ref.putFile(imguri)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // Get a URL to the uploaded content
-                      //  Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                        Toast.makeText(imageupload.this,"Image uploades successfully",Toast.LENGTH_LONG).show();
-                     }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Handle unsuccessful uploads
-                        // ...
-                    }
-                });
+
 
     }
-    private void Filechooser()
+    private void openFileChooser()
     {
         Intent intent=new Intent();
-        intent.setType("image/'");
+        intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent,1);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
+    }
+
+    private String getFileExtension(Uri uri)
+    {
+        ContentResolver cR=getContentResolver();
+        MimeTypeMap mime=MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+
+    private void uploadFile()
+    {
+        if(mImageUri !=null)
+        {
+        StorageReference fileReference=mStorageRef.child(System.currentTimeMillis()+"."+getFileExtension(mImageUri));
+
+        mUploadTask= fileReference.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+            {
+                Handler handler=new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mProgressBar.setProgress(0);
+                    }
+                },500);
+                Toast.makeText(imageupload.this,"Upload successfully",Toast.LENGTH_SHORT).show();
+
+                Upload upload=new Upload(mTextFileName.getText().toString().trim(),taskSnapshot.getUploadSessionUri().toString());
+                String uploadId=mDatabaseRef.push().getKey();
+                mDatabaseRef.child(uploadId).setValue(upload);
+
+            }
+        }
+        ).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+              Toast.makeText(imageupload.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        double progress =(100.0*snapshot.getBytesTransferred()/snapshot.getTotalByteCount());
+                        mProgressBar.setProgress((int)progress);
+                    }
+                });
+        }
+        else
+        {
+            Toast.makeText(this,"No file selected",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==1 && resultCode==RESULT_OK && data !=null && data.getData()!=null)
+        if(requestCode==PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data !=null && data.getData() !=null)
         {
-            imguri=data.getData();
-            img.setImageURI(imguri);
+            mImageUri=data.getData();
+            mImageView.setImageURI(mImageUri);
         }
     }
-
-   /* public void getquantity(View view) {
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.kg:
-                if(checked)
-                    str = "kg";
-                break;
-            case R.id.litre:
-                if(checked)
-                    str = "l";
-                break;
-            case R.id.other:
-                if(checked)
-                    str = "no";
-                break;
-            case R.id.meter:
-                if(checked)
-                    str = "m";
-                break;
-        }
-        //Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
-    }*/
 }
